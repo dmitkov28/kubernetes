e48628f33c161e3c628c603d73e3db7252a0e25e
dmitkov28 <mitkov.dimitar@yahoo.com>
2023-11-09 12:43:15 +0200

    add scripts for nfs

--- task_2/setup_scripts/nfs_client.sh
+++ task_2/setup_scripts/nfs_client.sh
@@ -2,3 +2,7 @@
 
 echo '* Install nfs client ...'
 apt update && apt install -y nfs-common
+
+echo '* Create & mount common directory ...'
+mkdir /nfs_data
+sudo mount 192.168.200.103:/nfs_data /nfs_data

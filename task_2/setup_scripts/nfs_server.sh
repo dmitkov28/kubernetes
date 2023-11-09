363c6db9ae19d11547aee964889d59d437c8771c
dmitkov28 <mitkov.dimitar@yahoo.com>
2023-11-09 19:01:49 +0200

    edit nfs_server.sh

--- task_2/setup_scripts/nfs_server.sh
+++ task_2/setup_scripts/nfs_server.sh
@@ -6,8 +6,8 @@ mkdir /nfs_data
 echo '* Install nfs server ...'
 apt update && apt install -y nfs-kernel-server
 
-echo '* Edit nfs config ...'
-echo '/nfs_data 192.168.200.0/255.255.255.0(rw,sync,no_subtree_check' >> /etc/exports
+echo '* Edit /etc/exports ...'
+echo '/nfs_data 192.168.200.0/255.255.255.0(rw,sync,no_subtree_check)' >> /etc/exports
 
 echo '* Restart nfs server ...'
 systemctl restart nfs-kernel-server
